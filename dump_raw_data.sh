47088d81d0212b11f10b204b9deee03fb5958248
Simon Gustafsson <simong@simong.se>
2016-07-13 13:15:33 +0200

    Proper start of scan of raw capacitance values in test mode.
    Read raw data should now be the raw data.
    
    Previously, when scan aquired in normal operating mode, the data we got
    was already compensated for background offset (i.e. autocalibration would
    have touched the data).

--- dump_raw_data.sh
+++ dump_raw_data.sh
@@ -17,54 +17,19 @@ I2C_BUS=1
 I2C_ADDR=0x38
 
 
-#
-# NOTE: Can't figure out how to start the scan manually
-#       (if that is even possible with the provided firmware)
-#
-## Set test mode
-#i2cset -y ${I2C_BUS} ${I2C_ADDR} 0x00 0x40 b || echo "ERROR-1"
-#sleep 0.1
-#
-## Start scan (will be set to zero when scan finishes)
-#i2cset -y ${I2C_BUS} ${I2C_ADDR} 0x02 0x08 b || echo "ERROR-2"
-#while true ; do
-#  VAL=$( i2cget -y ${I2C_BUS} ${I2C_ADDR} 0x02 b )
-#  [ $VAL == "0" ] && break;
-#  echo "VAL=$VAL"
-#done
-
-
-
-#
-# Workaround is to set the normal scanning mode, and pretend we are interested in touch events...
-#
-
-# Set normal mode
-i2cset -y ${I2C_BUS} ${I2C_ADDR} 0x00 0x00 b || echo "ERROR-1"
-sleep 0.1
-
-# Outputs a decimal number with current number of concurrent touches
-read_num_touches()
-{
-  local HEX_WITH_0x_PREFIX=$(i2cget -y ${I2C_BUS} ${I2C_ADDR} 0x02 b)
-  echo $((${HEX_WITH_0x_PREFIX}))
-}
-
-
-# Fake caring about touch events for 10 frames
-for N in $(seq 1 10) ; do
-  NUM_TOUCHES=$(read_num_touches)
-  [ "$NUM_TOUCHES" == 1 ] && {
-    i2cdump -y -r 3-6 1 0x38 bc > /dev/null
-  }
-  sleep 0.05
-done
-
-
-
 # Set test mode
 i2cset -y ${I2C_BUS} ${I2C_ADDR} 0x00 0x40 b || echo "ERROR-1"
 sleep 0.1
+
+# Start scan (by setting high bit while in test mode)
+i2cset -y ${I2C_BUS} ${I2C_ADDR} 0x00 0xc0 b || echo "ERROR-2"
+
+# Wait for scan to complete (wait for high bit to be cleared)
+while true ; do
+  VAL=$( i2cget -y ${I2C_BUS} ${I2C_ADDR} 0x00 b )
+  [ $VAL == "0x40" ] && break;
+  #echo "VAL=$VAL"
+done
 
 # Read out raw data
 for ROW_ADDR in $(seq 0 23) ; do
